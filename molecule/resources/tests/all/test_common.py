df67414ab3a70ce6cc8641db082d5b2d43d9a005
Nico Vaatstra <nico.vaatstra@open-xchange.com>
2020-09-16 17:24:35 +0200

    Additional tests to verify correct functioning of systemd service overrides

--- molecule/resources/tests/all/test_common.py
+++ molecule/resources/tests/all/test_common.py
@@ -1,3 +1,5 @@
+import re
+
 debian_os = ['debian', 'ubuntu']
 rhel_os = ['redhat', 'centos']
 
@@ -42,3 +44,24 @@ def test_tcp(host):
 def test_udp(host):
     udp = host.socket('udp://127.0.0.1:5300')
     assert udp.is_listening
+
+
+def test_service_overrides(host):
+    smgr = host.ansible("setup")["ansible_facts"]["ansible_service_mgr"]
+    if smgr == 'systemd':
+        fname = '/etc/systemd/system/dnsdist.service.d/override.conf'
+        f = host.file(fname)
+
+        assert f.exists
+
+        f_string = f.content.decode()
+
+        assert re.search(r'^LimitCORE=infinity$', f_string, re.MULTILINE) is not None
+
+        # # Ensure a ExecStart override is preceeded by a 'ExecStart=' reset instruction
+        if re.search(r'^ExecStart=.+$', f_string, re.MULTILINE) is not None:
+            assert re.search(r'^ExecStart=$(\r?\n)^ExecStart=.+$', f_string, re.MULTILINE) is not None
+        
+        # # Ensure a ExecStartPre override is preceeded by a 'ExecStartPre=' reset instruction
+        if re.search(r'^ExecStartPre=.+$', f_string, re.MULTILINE) is not None:
+            assert re.search(r'^ExecStartPre=$(\r?\n)^ExecStartPre=.+$', f_string, re.MULTILINE) is not None
